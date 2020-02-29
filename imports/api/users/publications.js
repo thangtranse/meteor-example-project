c34d9b28eeb5911e69b7b4431e992c2ddaf6ab0d
thangtm13 <thangtran.se@gmail.com>
2020-02-29 21:38:52 +0700

    complete realtime

--- imports/api/users/publications.js
+++ imports/api/users/publications.js
@@ -2,5 +2,5 @@ import {Meteor} from 'meteor/meteor';
 import {Users} from "../../../db/index";
 
 Meteor.publish('Users', () => {
-    return Users.find();
+    return Users.find({});
 });
--- imports/ui/pages/Users/ManagerUser.jsx
+++ imports/ui/pages/Users/ManagerUser.jsx
@@ -1,25 +1,69 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
+import MUIDataTable from "mui-datatables";
+
+
 import {withStyles} from '@material-ui/core/styles';
-import { useTracker } from 'meteor/react-meteor-data';
-import { Meteor } from 'meteor/meteor';
+import {useTracker} from 'meteor/react-meteor-data';
+import {Meteor} from 'meteor/meteor';
+
+import {Users} from '../../../../db/index';
 
 const styles = theme => ({});
+const columns = [
+    {
+        name: "_id",
+        label: "ID"
+    },
+    {
+        name: "email",
+        label: "Email"
+    },
+    {
+        name: "profile.firstName",
+        label: "First Name"
+    },
+    {
+        name: "profile.lastName",
+        label: "Last Name"
+    }
+];
+
+const options = {
+    filterType: 'checkbox'
+};
 
 function ManagerUser(props) {
     const {classes, children, content} = props;
+    const currentUser = useTracker(() => {
+        const checkSub = Meteor.subscribe("Users");
+        const listUsers = Users.find({}).fetch();
+        const UserID = Meteor.userId();
 
-    const listUser = useTracker(() => {
+        return {
+            isLoading: !checkSub.ready(),
+            listUsers,
+            UserID
+        }
+    });
 
-        Meteor.subscribe('Users')
+    currentUser.listUsers.map((first, indexFirst) => {
+        first.email = first.emails[0].address;
+        return first;
+    });
 
-    }, []);
 
-    console.log(listUser);
+    console.log("listUser", currentUser.listUsers);
+
+
     return (
-        <div>
-            manager User
-            {listUser}
-        </div>
+        <>
+            <MUIDataTable
+                title={"Danh sách User"}
+                data={currentUser.listUsers}
+                columns={columns}
+                options={options}
+            />
+        </>
     )
 }
 
